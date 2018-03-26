0d67634af5b7bb0cd8ff037ee0720d4b09d173fa
kartik <chauhan.kartik25@gmail.com>
2018-03-26 10:14:07 +0530

    dropdown added to recipe-details component

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+    'selector': '[appDropdown]'
+})
+
+export class DropdownDirective
+{
+    @HostBinding('class.open') isOpen: boolean = false;
+
+    @HostListener('click') toggleClass()
+    {
+        this.isOpen = !this.isOpen;
+    }
+}
